89a947e8997e011c5928bb03badf2483f0152d1d
郭家秀 <a880318@gmail.com>
2022-03-30 16:16:59 +0800

    feat: controller & view

---
+++ 0330_Auth/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using _0330_Auth.Models.DTO.Account;
+using _0330_Auth.Models.ViewModel.Account.Data;
+using _0330_Auth.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _0330_Auth.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAccountService _serevice;
+        public AccountController(IAccountService service)
+        {
+            _serevice = service;
+        }
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Signup(SignupDataModel request)
+        {
+            var inputDto = new CreateAccountInputDto
+            {
+                Email = request.Email,
+                Name = request.Email,
+                Phone = request.Phone,
+                Password = request.Password,
+                PasswordCheck = request.PasswordCheck,
+            };
+
+            var outputDto = _serevice.CreateAccount(inputDto);
+
+            if (outputDto.IsSuccess)
+            {
+                return Redirect("/");
+            }
+            else
+            {
+                return View("Signup");
+            }
+        }
+    }
+}
---
+++ 0330_Auth/Models/ViewModel/Account/Data/SignupDataModel.cs
@@ -0,0 +1,11 @@
+﻿namespace _0330_Auth.Models.ViewModel.Account.Data
+{
+    public class SignupDataModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+        public string PasswordCheck { get; set; }
+    }
+}
