7476ef320e9ce27fcb54dbf8b58557fe74b6c234
郭家秀 <a880318@gmail.com>
2022-03-30 14:47:59 +0800

    加入專案檔案。

---
+++ 0330_Auth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _0330_Auth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
