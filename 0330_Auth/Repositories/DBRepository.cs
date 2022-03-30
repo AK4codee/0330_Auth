ef39d95ed0b91a4f246a1ee164cbb082e3997160
郭家秀 <a880318@gmail.com>
2022-03-30 15:05:12 +0800

    feat: DBrepository

---
+++ 0330_Auth/Repositories/DBRepository.cs
@@ -0,0 +1,42 @@
+﻿using _0330_Auth.Models.DBEntity;
+using _0330_Auth.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace _0330_Auth.Repositories
+{
+    public class DBRepository : IDBRepository
+    {
+        private readonly AuthDBContext _dbContext;
+        public DBRepository(AuthDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public AuthDBContext DBContext { get { return _dbContext; } }
+
+        public void Create<T>(T entity) where T : class
+        {
+            _dbContext.Entry(entity).State = EntityState.Added;
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            _dbContext.Entry(entity).State = EntityState.Deleted;
+        }
+
+        public IQueryable<T> GetAll<T>() where T : class
+        {
+            return _dbContext.Set<T>();
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void Update<T>(T entity) where T : class
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
---
+++ 0330_Auth/Repositories/Interface/IDBRepository.cs
@@ -0,0 +1,15 @@
+﻿using _0330_Auth.Models.DBEntity;
+using System.Linq;
+
+namespace _0330_Auth.Repositories.Interface
+{
+    public interface IDBRepository
+    {
+        public AuthDBContext DBContext { get; }
+        public void Create<T>(T entity)  where T : class;
+        public void Update<T>(T entity) where T : class;
+        public void Delete<T>(T entity) where T : class;
+        public IQueryable<T> GetAll<T>() where T : class;
+        public void Save();
+    }
+}
